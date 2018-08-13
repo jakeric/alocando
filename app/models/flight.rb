f469e9df29c84d5fe3ca8ecc1d6acc49aa44e441
Kaj Schattner <38523893+jakeric@users.noreply.github.com>
2018-08-13 17:07:12 +0100

    Update flight.rb
    wrong syntax for the validation

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,11 +4,11 @@ class Flight < ApplicationRecord
   has_many :flight_bundle_flights
   belongs_to :airline
 
-  validates :departure_datetime, presence :true
-  validates :arrival_datetime, presence :true
-  validates :price, presence :true
-  validates :flight_duration, presence :true
-  validates :from_airport_id, presence :true
-  validates :to_airport_id, presence :true
+  validates :departure_datetime, presence: true
+  validates :arrival_datetime, presence: true
+  validates :price, presence: true
+  validates :flight_duration, presence: true
+  validates :from_airport_id, presence: true
+  validates :to_airport_id, presence: true
 
 end
