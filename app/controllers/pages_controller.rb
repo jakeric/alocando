b11a7cde15ca3e03372a65081d442486ef11e215
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-14 12:35:45 +0100

    created action and view for faq terms and about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
   end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def faq
+  end
+
 end
