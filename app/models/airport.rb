1af0977d0bb3bd4daaf7596f4e5db7097998daa3
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-15 11:44:53 +0100

    comment all validations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -3,6 +3,6 @@ class Airport < ApplicationRecord
   has_many :from_airports, :class_name => 'Flight', :foreign_key => :from_airport_id
   has_many :to_airports, :class_name => 'Flight', :foreign_key => :to_airport_id
 
-  validates :name, presence: true
-  validates :acronym, presence: true
+  # validates :name, presence: true
+  # validates :acronym, presence: true
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,7 @@ class City < ApplicationRecord
   has_many :airports
   has_many :attachments
 
-  validates :name, presence: true
-  validates :country, presence: true
+  # validates :name, presence: true
+  # validates :country, presence: true
   # validates :description, presence: true
 end
