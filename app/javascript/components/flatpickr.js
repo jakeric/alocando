cdb3f758f560ee1b6aed4dd1ba0eeec03e68abc1
Scarlett Wolter <scarlett.wolter@web.de>
2018-08-15 14:13:47 +0100

    datepicker

---
+++ app/javascript/components/flatpickr.js
@@ -0,0 +1,18 @@
+import flatpickr from 'flatpickr';
+
+const toggleDateInputs = function() {
+  const startDateinput = document.getElementById('inlineFormStartInputDate');
+  const endDateinput = document.getElementById('inlineFormEndInputDate');
+
+  flatpickr(startDateinput, {
+    mode: "range",
+    minDate: 'today',
+    dateFormat: "Y-m-d",
+    onChange: function(_, selectedDate) {
+      let splitted = selectedDate.split(" to ");
+      startDateinput.value = splitted[0];
+      endDateinput.value = splitted[1];
+    }});
+};
+
+export { toggleDateInputs }
