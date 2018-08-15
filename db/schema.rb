49c0242fb20ef07ce57f314e0b99740e567739f0
Scarlett Wolter <scarlett.wolter@web.de>
2018-08-15 17:26:35 +0100

    error message

--- db/schema.rb
+++ db/schema.rb
@@ -87,14 +87,6 @@ ActiveRecord::Schema.define(version: 2018_08_14_152121) do
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string "photo"
-    t.bigint "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_photos_on_city_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
