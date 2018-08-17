691c24b48492f82dccf2bd776ea6c450c4f3013b
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-17 15:40:45 +0100

    added bundle card styling

--- db/schema.rb
+++ db/schema.rb
@@ -77,22 +77,14 @@ ActiveRecord::Schema.define(version: 2018_08_14_152121) do
     t.datetime "arrival_datetime"
     t.float "price"
     t.float "flight_duration"
-    t.bigint "from_airport_id_id"
-    t.bigint "to_airport_id_id"
+    t.bigint "from_airport_id"
+    t.bigint "to_airport_id"
     t.bigint "airline_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["airline_id"], name: "index_flights_on_airline_id"
-    t.index ["from_airport_id_id"], name: "index_flights_on_from_airport_id_id"
-    t.index ["to_airport_id_id"], name: "index_flights_on_to_airport_id_id"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.string "photo"
-    t.bigint "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_photos_on_city_id"
+    t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
+    t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -117,5 +109,4 @@ ActiveRecord::Schema.define(version: 2018_08_14_152121) do
   add_foreign_key "flight_bundle_flights", "flight_bundles"
   add_foreign_key "flight_bundle_flights", "flights"
   add_foreign_key "flights", "airlines"
-  add_foreign_key "photos", "cities"
 end
