435d790e87dd6a1b74fc5c99208fc8332d992dc1
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-22 18:04:38 +0100

    added the city pictures seed

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  # belongs_to :city
+  belongs_to :city
   # JULIO: I commented line 3 because I just followed the lecture
   # lecture: https://kitt.lewagon.com/knowledge/lectures/05-Rails%2F05-Rails-MC-with-images#source
 end
