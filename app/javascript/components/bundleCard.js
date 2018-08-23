b7c85d8bfaf3f3593eba734c6a187932b314f74f
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-23 16:33:14 +0100

    fixed dashed line length in show

--- app/javascript/components/bundleCard.js
+++ app/javascript/components/bundleCard.js
@@ -1,3 +1,14 @@
+function activeCard(item) {
+  item.classList.add("bundle_selected");
+};
 
+
+const cardList = document.querySelectorAll('#background');
+let i = 0
+cardList.forEach(element => {
+  element.classList.add(`card${i}`)
+  let link = element.querySelector('#outline')
+  link.addEventListener("click", activeCard(element))
+});
 
 
