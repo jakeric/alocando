27acbab14f7524ccc2e54ea792842facd22fb437
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-23 12:05:09 +0100

    put the basis for JS events in index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 import "./application.css";
 import { toggleDateInputs } from "../components/flatpickr";
 import "../components/awesomplete";
-import "../components/bundleCard"
+import "../components/bundlecard"
 
 toggleDateInputs();
 
