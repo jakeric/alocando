6e7cb18ed811f14a1943242d2e5ed95fc36b89cb
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-23 19:04:24 +0100

    commented js doc for expansion

--- app/javascript/components/bundleCard.js
+++ app/javascript/components/bundleCard.js
@@ -1,14 +1,26 @@
-function activeCard(item) {
-  item.classList.add("bundle_selected");
-};
+// function activeCard(item) {
+//   item.classList.add("bundle_selected");
+// };
 
 
-const cardList = document.querySelectorAll('#background');
-let i = 0
-cardList.forEach(element => {
-  element.classList.add(`card${i}`)
-  let link = element.querySelector('#outline')
-  link.addEventListener("click", activeCard(element))
-});
+// const cardList = document.querySelectorAll('.background');
+
+// let ii = 0
+// cardList.forEach(element => {
+//   //element.classList.add(`card${i}`);
+//   let link = element.querySelector('.outline');
+//   link.addEventListener("click", activeCard(element));
+//   ii = ii + 1;
+//   console.log(ii)
+// });
+
+
+
+// document.querySelector('card')
+
+// let outlineList = document.querySelectorAll('#outline');
+// outlineList.forEach(item => {
+//   link.addEventListener("click", activeCard(item));
+// });
 
 
